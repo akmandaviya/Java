c51a85e5d25186484fdbfb6ad466ac0102526c59
akmandaviya <abdulmandaviya@gmail.com>
2022-08-12 18:00:29 +0530

    new commit

---
+++ basics_class_obj/main.java
@@ -0,0 +1,23 @@
+package basics_class_obj;
+
+class A {
+    int data; //member data/variable
+
+    A(int data) {
+         this.data = data; // constructor --> not a member
+    }
+
+    public void print() {
+        System.out.println(data); // printing
+    }
+}
+
+public class main {
+    public static void main(String[] args) {
+         A obj = new A(10);
+         obj.print();  //accessing member function
+         System.out.println(obj.data); //accessing member data/var 
+    }
+}
+
+//member fucntion and data can only be accessed by object
